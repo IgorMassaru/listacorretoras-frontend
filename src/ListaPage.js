c28739c8829ff4fafd99b985441c11ac456d2528
IgorMassaru <igormassaruy@gmail.com>
2020-06-03 15:56:14 +0000

    Versão com delete adicionada

--- src/ListaPage.js
+++ src/ListaPage.js
@@ -1,14 +1,18 @@
 import React, { useMemo, useState } from 'react';
 import Header from './Header';
 import api from './Api';
-import { Table, TableRow, TableCell, Switch, Button } from '@material-ui/core';
+import { Table, TableRow, TableCell, Switch, Button,Dialog,DialogTitle, DialogContent, TextField,DialogActions,DialogContentText } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 
 function ListaPage() {
 
     const [corretoras, setCorretoras] = useState([]);
-
+    const[open,setOpen] = useState(false);
+    const[nomecorretora,setNomeCorretora] = useState('');
+    const[ativo,setAtivo] = useState('');
+    const[valorporoperacao,setValorporoperacao]= useState('');
+    
     async function loadData() {
 
         const response = await api.get('/').then(response => {
@@ -18,6 +22,33 @@ function ListaPage() {
     }
 
     useMemo(loadData, []);
+    
+    function openDialog(){
+        setOpen(true);
+    }
+
+    function closeDialog(){
+        setOpen(false);
+    }
+
+    async function salvar(){
+        await api.post('/',{nomecorretora,ativo,valorporoperacao});
+        loadData();
+        setNomeCorretora('');
+        setAtivo('');
+        setValorporoperacao(1);
+    }
+
+    async function apagar(id){
+        await api.delete(`/${id}`);
+        loadData();
+    }
+     async function atualizar(id){
+        await api.put(`/${id}`);
+        loadData();
+    }
+
+
     return (
         <>
             <Header />
@@ -31,8 +62,11 @@ function ListaPage() {
                             <TableCell>{item.ativo}</TableCell>
                             <TableCell>{item.valorporoperacao}</TableCell>
                             <TableCell>
-                                <Switch checked={item.comprado} color="primary" />
-                                <Button variant="outlined" color="secondary" size="small">
+                                
+                                <Button variant="outlined"
+                                color="secondary" 
+                                size="small"
+                                onClick={() => apagar(item.id)}>
                                     <DeleteIcon />Apagar
                                     </Button>
                             </TableCell>
@@ -41,7 +75,51 @@ function ListaPage() {
                 }
             </Table>
 
-            <Button></Button>
+            <Button 
+                onClick={openDialog} 
+                variant ="contained" 
+                color="primary">Adicionar</Button>
+            
+            <Dialog open={open} onClose={closeDialog}>
+                <DialogTitle>Nova Corretora</DialogTitle>
+                <DialogContent>Cadastre sua nova corretora:
+                <TextField
+                autoFocus
+                margin="dense"
+                id="namecorretora"
+                label="Nome Corretora"
+                type="email"
+                fullWidth
+                value={nomecorretora}
+                onChange={e => setNomeCorretora(e.target.value)}
+                />
+                <TextField
+                autoFocus
+                margin="dense"
+                id="ativo"
+                label="Nome Ativo"
+                type="email"
+                fullWidth
+                value={ativo}
+                onChange={e => setAtivo(e.target.value)}
+                />
+                <TextField
+                autoFocus
+                margin="dense"
+                id="valorporoperacao"
+                label="Valor por Operacao"
+                type="email"
+                fullWidth
+                value={valorporoperacao}
+                onChange={e => setValorporoperacao(e.target.value)}
+                />
+
+                </DialogContent>
+                    <DialogActions>
+                        <Button onClick={closeDialog}>Cancelar</Button>
+                        <Button onClick={salvar}>Salvar</Button>
+                    </DialogActions>
+            </Dialog>
         </>
     )
 }
