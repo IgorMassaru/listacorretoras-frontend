fcb7a96d504bab4edd710f51764a954a702cc2d8
Henrique Dezani <dezani@gmail.com>
2020-06-10 13:01:09 +0000

    Adicionada função para editar dados.

--- src/ListaPage.js
+++ src/ListaPage.js
@@ -1,18 +1,19 @@
 import React, { useMemo, useState } from 'react';
 import Header from './Header';
 import api from './Api';
-import { Table, TableRow, TableCell, Switch, Button,Dialog,DialogTitle, DialogContent, TextField,DialogActions,DialogContentText } from '@material-ui/core';
+import { Table, TableRow, TableCell, Switch, Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions, DialogContentText } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 
 function ListaPage() {
 
-    const [corretoras, setCorretoras] = useState([]);
-    const[open,setOpen] = useState(false);
-    const[nomecorretora,setNomeCorretora] = useState('');
-    const[ativo,setAtivo] = useState('');
-    const[valorporoperacao,setValorporoperacao]= useState('');
-    
+    const [ corretoras, setCorretoras] = useState([]);
+    const [ open, setOpen] = useState(false);
+    const [ nomecorretora, setNomeCorretora] = useState('');
+    const [ ativo, setAtivo] = useState('');
+    const [ valorporoperacao, setValorporoperacao] = useState('');
+    const [ id, setId ] = useState(0);
+
     async function loadData() {
 
         const response = await api.get('/').then(response => {
@@ -22,30 +23,41 @@ function ListaPage() {
     }
 
     useMemo(loadData, []);
-    
-    function openDialog(){
+
+    function openDialog() {
         setOpen(true);
     }
 
-    function closeDialog(){
+    function closeDialog() {
         setOpen(false);
     }
 
-    async function salvar(){
-        await api.post('/',{nomecorretora,ativo,valorporoperacao});
+    async function salvar() {
+        if(id === 0) {
+            await api.post('/', { nomecorretora, ativo, valorporoperacao });
+        }
+        else {
+             await api.put(`/${id}`, { nomecorretora, ativo, valorporoperacao });
+        }
+
         loadData();
+        setId(0);
         setNomeCorretora('');
         setAtivo('');
         setValorporoperacao(1);
+        closeDialog();
     }
 
-    async function apagar(id){
+    async function apagar(id) {
         await api.delete(`/${id}`);
         loadData();
     }
-     async function atualizar(id){
-        await api.put(`/${id}`);
-        loadData();
+    async function editar(item) {
+        setId(item.id);
+        setNomeCorretora(item.nomecorretora);
+        setAtivo(item.ativo);
+        setValorporoperacao(item.valorporoperacao);
+        openDialog();
     }
 
 
@@ -62,63 +74,70 @@ function ListaPage() {
                             <TableCell>{item.ativo}</TableCell>
                             <TableCell>{item.valorporoperacao}</TableCell>
                             <TableCell>
-                                
                                 <Button variant="outlined"
-                                color="secondary" 
-                                size="small"
-                                onClick={() => apagar(item.id)}>
+                                    color="secondary"
+                                    size="small"
+                                    onClick={() => apagar(item.id)}>
                                     <DeleteIcon />Apagar
                                     </Button>
+                            </TableCell>
+                            <TableCell>
+                                <Button variant="outlined"
+                                    color="primary"
+                                    size="small"
+                                    onClick={() => editar(item)}>
+                                    Editar
+                                </Button>
                             </TableCell>
                         </TableRow>
                     ))
                 }
             </Table>
 
-            <Button 
-                onClick={openDialog} 
-                variant ="contained" 
+            <Button
+                onClick={openDialog}
+                variant="contained"
                 color="primary">Adicionar</Button>
-            
+
             <Dialog open={open} onClose={closeDialog}>
-                <DialogTitle>Nova Corretora</DialogTitle>
-                <DialogContent>Cadastre sua nova corretora:
+                <DialogTitle>{id === 0 ? 'Nova' : 'Editar'} Corretora</DialogTitle>
+                <DialogContent>{id === 0 ? 'Cadastre' : 'Edita'} sua corretora:
                 <TextField
-                autoFocus
-                margin="dense"
-                id="namecorretora"
-                label="Nome Corretora"
-                type="email"
-                fullWidth
-                value={nomecorretora}
-                onChange={e => setNomeCorretora(e.target.value)}
-                />
-                <TextField
-                autoFocus
-                margin="dense"
-                id="ativo"
-                label="Nome Ativo"
-                type="email"
-                fullWidth
-                value={ativo}
-                onChange={e => setAtivo(e.target.value)}
-                />
-                <TextField
-                autoFocus
-                margin="dense"
-                id="valorporoperacao"
-                label="Valor por Operacao"
-                type="email"
-                fullWidth
-                value={valorporoperacao}
-                onChange={e => setValorporoperacao(e.target.value)}
-                />
+                        autoFocus
+                        margin="dense"
+                        id="namecorretora"
+                        label="Nome Corretora"
+                        type="email"
+                        fullWidth
+                        value={nomecorretora}
+                        onChange={e => setNomeCorretora(e.target.value)}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="ativo"
+                        label="Nome Ativo"
+                        type="email"
+                        fullWidth
+                        value={ativo}
+                        onChange={e => setAtivo(e.target.value)}
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="valorporoperacao"
+                        label="Valor por Operacao"
+                        type="email"
+                        fullWidth
+                        value={valorporoperacao}
+                        onChange={e => setValorporoperacao(e.target.value)}
+                    />
 
                 </DialogContent>
-                    <DialogActions>
-                        <Button onClick={closeDialog}>Cancelar</Button>
-                        <Button onClick={salvar}>Salvar</Button>
-                    </DialogActions>
+                <DialogActions>
+                    <Button onClick={closeDialog}>Cancelar</Button>
+                    <Button onClick={salvar}>{id === 0 ? 'Salvar' : 'Atualizar'}</Button>
+                </DialogActions>
             </Dialog>
         </>
     )
